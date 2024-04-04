bb8c79e3e4d8f35871db16f0dca82d1aecdb7556
kim-jun-young <jykim@gemiso.com>
2024-04-04 14:02:55 +0900

    check

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -9,21 +9,21 @@ export const API_URL = (path: string) => {
 
 
 
-export const GITHUB_API_URL: string = getEnv('GIT_HUB_API_URL');
+export const GITHUB_API_URL: string = getEnv('NEXT_PUBLIC_GIT_HUB_API_URL');
 export const GITHUB_OBSIDIAN_CONFIG: GithubObsidianConfig = {
-	token: getEnv('GIT_HUB_OBSIDIAN_TOKE'),
-	owner: getEnv('GIT_HUB_OBSIDIAN_OWNER'),
-	repo:  getEnv('GIT_HUB_OBSIDIAN_REPO'),
-	version:  getEnv('GIT_HUB_OBSIDIAN_API_VERSION'),
-	email:  getEnv('GIT_HUB_OBSIDIAN_EMAIL'),
-	name:  getEnv('GIT_HUB_OBSIDIAN_NAME'),
+	token: getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_TOKE'),
+	owner: getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_OWNER'),
+	repo:  getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_REPO'),
+	version:  getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_API_VERSION'),
+	email:  getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_EMAIL'),
+	name:  getEnv('NEXT_PUBLIC_GIT_HUB_OBSIDIAN_NAME'),
 }
 export const GITHUB_CONFIG: GithubConfig = {
-	token: getEnv('GIT_HUB_TOKE'),
-	owner: getEnv('GIT_HUB_OWNER'),
-	repo:  getEnv('GIT_HUB_REPO'),
-	version:  getEnv('GIT_HUB_API_VERSION'),
-	email:  getEnv('GIT_HUB_EMAIL'),
-	name:  getEnv('GIT_HUB_NAME'),
+	token: getEnv('NEXT_PUBLIC_GIT_HUB_TOKE'),
+	owner: getEnv('NEXT_PUBLIC_GIT_HUB_OWNER'),
+	repo:  getEnv('NEXT_PUBLIC_IT_HUB_REPO'),
+	version:  getEnv('NEXT_PUBLIC_GIT_HUB_API_VERSION'),
+	email:  getEnv('NEXT_PUBLIC_GIT_HUB_EMAIL'),
+	name:  getEnv('NEXT_PUBLIC_GIT_HUB_NAME'),
 }
 
