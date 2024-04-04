0701974ab692335736e93511728a0d04dcb8f09f
kim-jun-young <jykim@gemiso.com>
2024-04-04 10:55:24 +0900

    download url log

--- src/app/api/blog/[[...slug]]/route.ts
+++ src/app/api/blog/[[...slug]]/route.ts
@@ -34,7 +34,7 @@ export async function GET(request: Request, { params }: Params)
 
     const formattedData: BlogContentInterface[] = [];
     for(const item of data){
-
+        console.log(item);
         let content: BlogContentInterface['content'] = null;
         if(item.type === 'file'){
 
