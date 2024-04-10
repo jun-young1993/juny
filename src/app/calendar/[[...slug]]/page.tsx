7b0a0962158872f56f618208dbe2f48b898189e3
kim-jun-young <jykim@gemiso.com>
2024-04-10 13:00:02 +0900

    calendar container

--- src/app/calendar/[[...slug]]/page.tsx
+++ src/app/calendar/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import Calendar from "@/components/calendar/calendar";
 import ContainerLayout from "@/components/layouts/container.layouts"
 import { MenuType } from "@/types/menu.type"
 import { MultiSegmentPageParams } from "@/types/next.type"
@@ -12,9 +13,10 @@ export default async function Page({ params }: MultiSegmentPageParams){
 			type={MenuType.CALENDAR}
 			title={MenuType.CALENDAR}
 		>
-			{getDaysInMonth(2023,4).map((day) => {
+			<Calendar />
+			{/* {getDaysInMonth(2023,4).map((day) => {
 				return <div>${day.toString()}</div>
-			})}
+			})} */}
 		</ContainerLayout>
 	)
 }
---
+++ src/components/calendar/calendar.tsx
@@ -0,0 +1,15 @@
+export default function Calendar(){
+	return (
+		<div className="grid grid-cols-7 gap-4">
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>01</div>
+			<div>09</div>
+		</div>
+	)
+}
