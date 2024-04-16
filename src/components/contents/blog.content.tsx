65a59d0cc3af38edf8f25f2480b8d9c1d61cffea
kim-jun-young <jykim@gemiso.com>
2024-04-16 18:44:55 +0900

    blog content

--- src/components/contents/blog.content.tsx
+++ src/components/contents/blog.content.tsx
@@ -1,34 +1,16 @@
 'use client';
 import { BlogPath, _BLOG_CONTENT_TYPE } from "@/defined/blog.defined";
-import { BlogContentInterface } from "@/types/blog.type"
+import { BlogContentInterface, BlogContentProps } from "@/types/blog.type"
 import Link from "next/link";
 import { ReactNode, useState } from "react";
 import { FileIcon, FolderIcon } from "../icons/svg.icon";
 import _ from "lodash";
-
-interface BlogContentModalInterface {
-	link?: (content: BlogContentInterface,index: number) => ReactNode
-	selectedIndex?: number
-	onNavigationClick?: (path: string, index: number) => void
-}
-
-interface BlogContentProps {
-	data: BlogContentInterface[] | []
-	path: string
-	modal?: BlogContentModalInterface
-}
-
-function generatePaths(str: string): string[]
-{
-	const parts = str.split('/');
-	const result = parts.map((_, index) => parts.slice(0, index + 1).join(' / '));
-	return result;
-}
+import { BlogContentMenuNav } from "../blog/blog-content-menu-nav";
 
 export default function BlogContent(props: BlogContentProps){
 
 	const [selectedIndex, setSelectedIndex] = useState<number>(0);
-	const navClassName:string = "ml-1 hover:text-bold hover:underline hover:underline-offset-8 decoration-sky-500 dark:decoration-sky-200 hover:capitalize";
+	
 
 	const handleClickMenuItem = (content: BlogContentInterface, index: number) => {
 		if(content.type === _BLOG_CONTENT_TYPE.FILE){
@@ -63,35 +45,11 @@ export default function BlogContent(props: BlogContentProps){
 
 	return (
 		<div className="flex h-[90%]">
-			<div className={`flex-none w-1/4 mini:w-full h-full ${isContent ? 'mini:hidden' : 'mini:block'}`}>
+			<div className={`flex-none medium:w-1/4 mini:w-full h-full ${isContent ? 'mini:hidden medium:block' : 'mini:block'}`}>
 				<ul className="menu bg-base-200 w-56 rounded-box dark:bg-gray-600 dark:text-gray-200 w-full">
 					<li className="w-full">
-						<h2 className="menu-title dark:text-gray-200 text-lg">
-							{(props?.modal?.onNavigationClick)
-							?<span onClick={() => {
-								if(props.modal?.onNavigationClick){
-									props?.modal?.onNavigationClick(BlogPath,0);
-								}
-							}} className={navClassName+"hover:cursor-pointer"} key={`${BlogPath}`}><span className="hover:cursor-pointer">{"blog".toLocaleUpperCase()}</span>/</span>
-							: <Link className={navClassName} href={BlogPath}>{"blog".toLocaleUpperCase()}</Link>
-							}
-							
-							{generatePaths(props.path ?? '').map((path,index) => {
-								const currentPath = props.path.split('/')[index];
-								const userPath = `${BlogPath}/${path}`;
-								if(_.isEmpty(path) || path === ''){
-									return null;
-								}
-								if(props?.modal?.onNavigationClick){
-									return <span onClick={() => {
-										if(props.modal?.onNavigationClick){
-											props?.modal?.onNavigationClick(userPath, index+1);
-										}
-									}} className={navClassName+"hover:cursor-pointer"} key={`${BlogPath} / ${path}`}><span className="hover:cursor-pointer">/{currentPath}</span></span>
-								}
-								return <Link className={navClassName} key={`${BlogPath} / ${path}`} href={userPath}>/{currentPath}</Link>
-							})}
-						</h2>
+						<BlogContentMenuNav {...props} />
+				
 						<ul>
 							{props.data.map((content,index) => {
 								return (
@@ -114,6 +72,7 @@ export default function BlogContent(props: BlogContentProps){
 				) &&
 					<div className="w-full h-full">
 						<div className="w-full flex justify-center h-1/12">
+							<BlogContent {...props} />
 							<div className="text-xl font-bold italic">{props.data[props?.modal?.selectedIndex ?? selectedIndex].name}</div>
 						</div>
 						<div className="w-full h-11/12 flex justify-center">
--- src/types/blog.type.ts
+++ src/types/blog.type.ts
@@ -1,5 +1,6 @@
 import { _BLOG_CONTENT_TYPE } from "@/defined/blog.defined"
 import { GithubContentInterface } from "./github.type"
+import { ReactNode } from "react"
 
 export interface BlogContentInterface extends GithubContentInterface{
 	name: string
@@ -7,4 +8,16 @@ export interface BlogContentInterface extends GithubContentInterface{
 	type: _BLOG_CONTENT_TYPE
 	content?: string
 	download_url: string
+}
+
+export interface BlogContentModalInterface {
+	link?: (content: BlogContentInterface,index: number) => ReactNode
+	selectedIndex?: number
+	onNavigationClick?: (path: string, index: number) => void
+}
+
+export interface BlogContentProps {
+	data: BlogContentInterface[] | []
+	path: string
+	modal?: BlogContentModalInterface
 }
