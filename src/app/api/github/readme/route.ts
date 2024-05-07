48f029ec88ae04e86099f822584e08d999debc44
jun-young1993 <juny3738@gmail.com>
2024-05-07 18:51:39 +0900

    init

--- src/app/api/github/readme/route.ts
+++ src/app/api/github/readme/route.ts
@@ -19,7 +19,7 @@ export async function GET(request: Request)
 	})
 
 
-
+	
 	if(res.status !== constants.HTTP_STATUS_OK){
 		throw new Error(res.statusText);
 	}
--- src/app/util/sitemap.ts
+++ src/app/util/sitemap.ts
@@ -1,12 +1,37 @@
-import {API_URL} from "@/lib/config/config";
+import {API_URL, GITHUB_API_URL, GITHUB_OBSIDIAN_CONFIG, ORIGIN} from "@/lib/config/config";
 import {MetadataRoute} from "next";
 
 async function getData(){
-    const res = await fetch(API_URL('/obsidian/repos/trees/sitemap'),{
-        method: 'GET',
-        next: { revalidate: 3600 }
-    })
-    const result = await res.json();
+    // const res = await fetch(API_URL('/obsidian/repos/trees/sitemap'),{
+    //     method: 'GET',
+    //     next: { revalidate: 3600 }
+    // })
+    // const result = await res.json();
+    // return result;
+
+    const url = `${GITHUB_API_URL}/repos/${GITHUB_OBSIDIAN_CONFIG.owner}/${GITHUB_OBSIDIAN_CONFIG.repo}/git/trees/main?recursive=true`
+    const res = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${GITHUB_OBSIDIAN_CONFIG.token}`,
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': GITHUB_OBSIDIAN_CONFIG.version
+        }
+    });
+    const jsonData = await res.json();
+    const tree = jsonData.tree;
+    const result = [];
+    
+    for(let index=0; index<tree.length; index++){
+        const currentData = tree[index];
+        if(currentData.path.startsWith('blog') && currentData.path.endsWith('.md')){
+            result.push({
+                url: `${ORIGIN}/${currentData.path}`,
+                lastModified: new Date(),
+                changeFrequency: 'yearly',
+                priority: 1,
+            })
+        }
+    }
     return result;
 }
 
