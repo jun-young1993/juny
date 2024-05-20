369c2d58b6ba84fd4607b8817cd370f46d8cc4bc
junyoungkim <juny3738@gmail.com>
2024-05-20 20:35:19 +0900

    comment

--- src/components/guest-book/guest-book.tsx
+++ src/components/guest-book/guest-book.tsx
@@ -1,12 +1,35 @@
 "use client";
 import {GithubIssueComment, GithubIssueReplyList} from 'react-github-issue';
 import ContainerLayout from "@/components/layouts/container.layouts";
+import {useSearchParams} from "next/navigation";
 const GuestBook = () => {
+    const searchParams = useSearchParams();
+    const gitOAuthCode = searchParams.get('code');
+    if(gitOAuthCode){
+        const accessTokenAuthUrl:string = "https://github.com/login/oauth/access_token";
+        console.log({
+            client_id: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_ID,
+            client_secret: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_SECRET,
+            code: gitOAuthCode,
+        });
+        fetch(accessTokenAuthUrl,{
+            method: "post",
+            body: JSON.stringify({
+                client_id: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_ID,
+                client_secret: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_SECRET,
+                code: gitOAuthCode,
+            })
+        })
+            .then((result) => {
+                console.log(result.json());
+            })
 
+    }
     return (
        <ContainerLayout>
            <GithubIssueComment
-               gitPersonalAccessToken={process.env.NEXT_PUBLIC_GIT_HUB_TOKEN}
+               // gitPersonalAccessToken={process.env.NEXT_PUBLIC_GIT_HUB_TOKEN}
+
            />
             <GithubIssueReplyList
                 gitPersonalAccessToken={process.env.NEXT_PUBLIC_GIT_HUB_TOKEN}
