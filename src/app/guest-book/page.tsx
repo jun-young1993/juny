a2fb4b6d33134f1ec1c62e95fd549591c32ff73a
junyoungkim <juny3738@gmail.com>
2024-05-20 23:28:07 +0900

    git oauth

--- src/app/guest-book/page.tsx
+++ src/app/guest-book/page.tsx
@@ -1,7 +1,34 @@
 import GuestBook from "@/components/guest-book/guest-book";
+import {useSearchParams} from "next/navigation";
+interface Params {
+    searchParams: {
+        code?: string
+    }
+}
+export default async function Page({
+    searchParams
+}: Params){
+    const gitOAuthCode = searchParams.code;
+    let personalAccessToken;
+    if(gitOAuthCode){
+        const accessTokenAuthUrl:string = "https://github.com/login/oauth/access_token";
+        const response = await fetch(accessTokenAuthUrl,{
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                client_id: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_ID,
+                client_secret: process.env.NEXT_PUBLIC_GIT_HUB_OAUTH_CLIENT_SECRET,
+                code: gitOAuthCode,
+                redirect: 'follow'
+            })
+        });
+        const result = await response.text();
+        const data = result.split("&");
+        const accessToken = data[0].split("=")[1];
+        personalAccessToken = accessToken;
+    }
 
-export default async function Page(){
-
-
-    return <GuestBook />;
+    return <GuestBook personalAccessToken={personalAccessToken}/>;
 }
