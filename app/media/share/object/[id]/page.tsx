9c69464c223f4ab9a4db713c8a30c474ba36194c
jun-young1993 <juny3738@gmail.com>
2025-11-26 18:42:45 +0900

    init

--- app/media/share/object/[id]/page.tsx
+++ app/media/share/object/[id]/page.tsx
@@ -15,7 +15,8 @@ export async function generateMetadata(props: PageProps): Promise<Metadata> {
   return genPageMetadata({
     title: shareMediaGroup.title,
     description:
-      shareMediaGroup.description || shareMediaGroup.s3Object.length + '개의 미디어를 공유합니다.',
+      'Share Code: ' + shareMediaGroup.shareCode + '\r\n' + shareMediaGroup.description ||
+      shareMediaGroup.s3Object.length + '개의 미디어를 공유합니다.',
     image: shareMediaGroup.s3Object[0].thumbnailUrl,
   })
 }
